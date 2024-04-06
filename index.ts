ab0058b1ae3102e656c7ab1dc0e4eab13da90a7c
naolleg <naollegesse51@gmail.com>
2024-04-06 22:26:09 +0300

    inital creating express server of index.ts

---
+++ index.ts
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+const indexroute = require('./routes/index.route');
+dotenv.config();
+
+
+
+const app = express();
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(indexroute);
+
+
+
+const PORT=process.env.PORT || 3033;
+
+app.listen(PORT, function () {
+    console.log(`Server listening on port ${PORT}`);
+});
+
