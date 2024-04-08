cdc7a925479905e6467ce80e9fad58c762aa3e63
naolleg <naollegesse51@gmail.com>
2024-04-08 16:48:01 +0300

    adding some adjustments on the cofiguration

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,16 @@
+"use strict";
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require('dotenv');
-// const indexroute = require('./routes/index.route');
+const indexroute = require('./routes/index.route');
 dotenv.config();
-
 console.log();
-
-
 const app = express();
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//app.use(indexroute);
-
-
-
-const PORT=process.env.PORT || 3033;
-
+app.use(indexroute);
+const PORT = process.env.PORT || 3033;
 app.listen(PORT, function () {
     console.log(`Server listening on port ${PORT}`);
 });
-
