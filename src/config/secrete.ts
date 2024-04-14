50a3d7fe7c2e800c6d9a72cbdfae6cc78b797587
naolleg <naollegesse51@gmail.com>
2024-04-14 20:49:25 +0300

    removing web token temporarily

--- src/config/secrete.ts
+++ src/config/secrete.ts
@@ -1,7 +1,7 @@
 const env=require('dotenv');
 env.config();
 
-const JWT_SECRET = process.env.JWT_SECRET || "se";
+;
 const PORT = process.env.PORT || 8888;
 const DATABASE_URL = process.env.DATABASE_URL;
 
