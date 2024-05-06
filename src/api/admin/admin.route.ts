58f350fd2f3d8cef37bd0181e46f5a5dd24e399b
naol legesse <naollegesse51@gmail.com>
2024-05-06 18:41:22 +0300

    admin route common js to es6

--- src/api/admin/admin.route.ts
+++ src/api/admin/admin.route.ts
@@ -1,8 +1,10 @@
 "use strict";
-const admincontroller = require("./admin.controller");
-const express = require("express");
+import admincontroller from "./admin.controller";
+import express from "express";
+
 const adminrouter = express.Router();
-// adminrouter.get("/dashboard",admincontroller.login);
+
 adminrouter.post("/login", admincontroller.login);
 adminrouter.post("/register", admincontroller.register);
-module.exports = adminrouter;
+
+export default adminrouter;
