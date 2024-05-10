d6d519f086ae96cadc07941c3a511783446b34ef
naol legesse <naollegesse51@gmail.com>
2024-05-10 20:15:30 +0300

    job controller adjustment

--- src/api/job/job.controller.ts
+++ src/api/job/job.controller.ts
@@ -1,89 +1,10 @@
 import { Request, Response } from "express";
+import { prisma } from "../../config/prisma.js";
 const jobController = {
   createPost: async (req:Request, res:Response) => {
-    try {
-      const {
-        title,
-        category,
-        salary,
-        experience,
-        qualification,
-        quantity,
-        region,
-        city,
-        orgProfileId,
-        createdDate,
-        deadline,
-        hirerName
-      } = req.body;
+   const newjob=await prisma
 
-      if (
-        !title ||
-        !category ||
-        !salary ||
-        !experience ||
-        !qualification ||
-        !quantity ||
-        !city ||
-        !region ||
-        !orgProfileId ||
-        !deadline ||
-        !hirerName
-      ) {
-        return res.status(400).json({
-          success: false,
-          message: "All fields are required"
-        });
-      }
 
-      const addressData = { region, city };
-      const isAddressPosted = await jobService.insertAddress(addressData);
-
-      if (!isAddressPosted) {
-        return res.status(400).json({
-          success: false,
-          message: "Address not posted"
-        });
-      }
-
-      const addressId = isAddressPosted.insertId;
-
-      const jobData = {
-        title,
-        category,
-        salary,
-        experience,
-        qualification,
-        quantity,
-        addressId,
-        orgProfileId,
-        createdDate,
-        deadline,
-        hirerName
-      };
-
-      const isJobPosted = await jobService.createPost(jobData);
-      console.log(isJobPosted.insertId);
-
-      if (isJobPosted) {
-        return res.status(200).json({
-          success: true,
-          message: "Job posted successfully"
-        });
-      }
-    } catch (error) {
-      throw error;
-    }
-  },
-
-  // Get all job posts
-  getAllPosts: async (req, res) => {
-    const result = await jobService.getAllPosts(req.body);
-    return res.status(200).json({
-      success: true,
-      data: result
-    });
   }
-};
-
+}
 export default jobController;
