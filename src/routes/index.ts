16f8a6ccd003cfa45e3fe75cc0482d9182b248c9
naol legesse <naollegesse51@gmail.com>
2024-05-11 16:23:57 +0300

    adding all the routes in the index and there midleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
-const userroute = require("../api/user/user.route");
-const adminroute = require("../api/admin/admin.route");
-const approute=express.Router();
+import express from "express";
+import userroute from "../api/user/user.route";
+import adminroute from "../api/admin/admin.route";
+import jobRoute from "../api/job/job.route";
+const approute = express.Router();
 
 approute.use(userroute);
 approute.use(adminroute);
 
-module.exports=approute;
-
+export default approute;
 
