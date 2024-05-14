380cc30296661f0465df287a21c6217c4b1aacab
naol legesse <naollegesse51@gmail.com>
2024-05-14 14:17:50 +0300

    some adjustment on the prisma schema and the index route adding role enumeration

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,17 @@
 "use strict";
-const express = require("express");
-const bodyParser = require("body-parser");
-const dotenv = require('dotenv');
-const indexroute = require('./src/routes/index');
-require('./src/config/secrete');
-const app = express();
-//middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(indexroute);
-app.listen(PORT, function () {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+require("./src/config/secrete");
+const app = (0, express_1.default)();
+const PORT = 3000; // Reemplaza 3000 con el número de puerto que desees
+// Middleware
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+// app.use(indexroute);
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
