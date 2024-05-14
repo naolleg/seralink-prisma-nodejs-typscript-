c09641529327d09a3a201ec29a2e3176900eef35
naol legesse <naollegesse51@gmail.com>
2024-05-14 14:46:49 +0300

    added jsonweb token in the env and secrete

--- src/config/secrete.ts
+++ src/config/secrete.ts
@@ -1,9 +1,9 @@
-const env=require('dotenv');
-env.config();
+import dotenv from 'dotenv';
+dotenv.config({path: '.env'});
 
-;
-const PORT = process.env.PORT || 8888;
-const DATABASE_URL = process.env.DATABASE_URL;
-
-module.exports={PORT,DATABASE_URL};
-
+export const PORT = process.env.PORT || 8888;
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const SECRET = process.env.JWT_SECRET
+export const EMAIL = process.env.EMAIL
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
+export const BASE_URL = process.env.BASE_URL;
