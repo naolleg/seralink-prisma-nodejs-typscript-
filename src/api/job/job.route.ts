d7498a3a23c0eca41bfdbc97e283e03f2be7051c
naol legesse <naollegesse51@gmail.com>
2024-05-18 23:52:04 +0300

    added crete job post route

--- src/api/job/job.route.ts
+++ src/api/job/job.route.ts
@@ -12,7 +12,7 @@ jobRoute.get('/', (req, res) => {
 //jobRoute.get('/posts', jobcontroller.getallposts);
 // jobRoute.get('/providers/posts', jobcontroller.getposts);
 // jobRoute.get('/posts/:id', jobController.getsinglepost);
-//jobRoute.post('/posts', jobcontroller.createpost);
+jobRoute.post('/posts', jobcontroller.createPost);
 // jobRoute.delete('/provider/posts/:id', jobcontroller.deletepost);
 // jobRoute.put('/api/posts/:id', jobController.updatepost);
 
